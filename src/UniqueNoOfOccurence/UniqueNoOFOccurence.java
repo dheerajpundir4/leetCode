5314dd93523413f5aac61d212d4ddeb8a89230ca
dheerajpundir4 <dheerajpundir4@gmail.com>
2022-05-26 10:29:39 +0530

    1207. Unique Number of Occurrences

---
+++ src/UniqueNoOfOccurence/UniqueNoOFOccurence.java
@@ -0,0 +1,46 @@
+package UniqueNoOfOccurence;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class UniqueNoOFOccurence {
+
+
+}
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+
+        HashMap<Integer, Integer> map=new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int num:arr){
+
+            if(map.containsKey(num)){
+
+                int value=map.get(num);
+                value++;
+
+                map.put(num,value);
+            }
+            else{
+
+                map.put(num,1);
+            }
+        }
+
+        System.out.println(map);
+
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+
+            if(set.contains(entry.getValue())){
+
+                return false;
+            }
+            set.add(entry.getValue());
+        }
+
+        return true;
+
+    }
+}
