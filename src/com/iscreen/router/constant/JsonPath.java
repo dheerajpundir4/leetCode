82ff2e21bd8797a0b5a9534d5fae97fe219a9cb3
dheerajpundir4 <dheerajpundir4@gmail.com>
2024-05-07 22:38:27 +0530

    remove some data

--- src/com/iscreen/router/constant/JsonPath.java
+++
@@ -1,7 +0,0 @@
-package com.iscreen.router.constant;
-
-public class JsonPath {
-    public static class PersonName{
-        public static final FIRST_NAME="/background/firstname";
-    }
-}
--- src/com/iscreen/router/constant/MandatoryFields.java
+++
@@ -1,8 +0,0 @@
-package com.iscreen.router.constant;
-
-public class MandatoryFields {
-    public static final List<String> mandatoryFields=List.of(
-            JsonPath.PersonalInfo.PersonName.FIRST_NAME;
-            JsonPath.PersonalInfo.PersonName.LAST_NAME;
-    );
-}
